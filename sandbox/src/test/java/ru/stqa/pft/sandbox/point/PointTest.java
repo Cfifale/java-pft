8f7c97b2f4066a2f0dcaf718bb3d30100e87f258
cfifale <cfifale@yandex.ru>
2020-02-10 22:54:59 +0300

    Задание №3 (попытка_2)

--- sandbox/src/test/java/ru/stqa/pft/sandbox/point/PointTest.java
+++ sandbox/src/test/java/ru/stqa/pft/sandbox/point/PointTest.java
@@ -4,17 +4,16 @@ import org.testng.Assert;
 import org.testng.annotations.Test;
 
 public class PointTest {
+    Point p1 = new Point(3,3);
+    Point p2 = new Point(5,5);
 
     @Test
     public void testPoint1() {
-        Point p1 = new Point(3,3);
-        Point p2 = new Point(5,5);
         Assert.assertEquals(ProgramPoint.distance(p1,p2),2.8284271247461903);
     }
 
     @Test
     public void testPoint2() {
-        Point p1p2 = new Point(3,3,5,5);
-        Assert.assertEquals(p1p2.distance(),2.8284271247461903);
+        Assert.assertEquals(p1.distance(p2),2.8284271247461903);
     }
 }
