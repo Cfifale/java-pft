5d8d619adbfd16716c50b877b283538260f57ac0
cfifale <cfifale@yandex.ru>
2020-02-10 22:54:18 +0300

    Задание №2 (попытка_2)

--- sandbox/src/main/java/ru/stqa/pft/sandbox/point/Point.java
+++ sandbox/src/main/java/ru/stqa/pft/sandbox/point/Point.java
@@ -6,24 +6,13 @@ import static java.lang.Math.sqrt;
 public class Point {
     public double x;
     public double y;
-    public double x2;
-    public double y2;
 
     public Point(double x, double y) {
         this.x = x;
         this.y = y;
     }
 
-    public Point(double x, double y, double x2, double y2) {
-        this.x = x;
-        this.y = y;
-        this.x2 = x2;
-        this.y2 = y2;
-    }
-
-    public double distance() {
-        return sqrt(pow(this.x2 - this.x,2) + pow(this.y2 - this.y,2));
+    public double distance(Point p) {
+        return sqrt(pow(p.x - this.x,2) + pow(p.y - this.y,2));
     }
 }
-
-
--- sandbox/src/main/java/ru/stqa/pft/sandbox/point/ProgramPoint.java
+++ sandbox/src/main/java/ru/stqa/pft/sandbox/point/ProgramPoint.java
@@ -8,8 +8,7 @@ public class ProgramPoint {
         Point p1 = new Point(3,-4);
         Point p2 = new Point(-6,5);
         System.out.println("Вычисление через функцию. Расстояние между двумя точками A("+p1.x+";"+p1.y+") и B("+p2.x+";"+p2.y+") = "+distance(p1,p2));
-        Point p1p2 = new Point(3,4,6,5);
-        System.out.println("Вычисление через метод. Расстояние между двумя точками A("+p1p2.x+";"+p1p2.y+") и B("+p1p2.x2+";"+p1p2.y2+") = "+p1p2.distance());
+        System.out.println("Вычисление через метод. Расстояние между двумя точками A("+p1.x+";"+p1.y+") и B("+p2.x+";"+p2.y+") = "+p1.distance(p2));
     }
 
     public static double distance(Point p1, Point p2) {
