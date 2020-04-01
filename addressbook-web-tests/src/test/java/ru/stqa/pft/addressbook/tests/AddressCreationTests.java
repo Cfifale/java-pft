e726b0feda8e933748f26ec4b8aefd2b1d7adcd7
cfifale <cfifale@yandex.ru>
2020-04-01 05:20:30 +0300

    Задание №13

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/AddressCreationTests.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/AddressCreationTests.java
@@ -1,25 +1,42 @@
 package ru.stqa.pft.addressbook.tests;
 
+import com.google.gson.Gson;
+import com.google.gson.reflect.TypeToken;
+import org.testng.annotations.DataProvider;
 import org.testng.annotations.Test;
 import ru.stqa.pft.addressbook.model.AddressData;
 import ru.stqa.pft.addressbook.model.Contacts;
 
+import java.io.BufferedReader;
 import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.Iterator;
+import java.util.List;
+import java.util.stream.Collectors;
 
 import static org.hamcrest.CoreMatchers.equalTo;
 import static org.hamcrest.MatcherAssert.assertThat;
 
 public class AddressCreationTests extends TestBase {
 
-    @Test
-    public void testsAddressCreation() {
+    @DataProvider
+    public Iterator<Object[]> validAddressFromJson() throws IOException {
+        BufferedReader reader = new BufferedReader(new FileReader(new File("src/test/resources/contacts.json")));
+        String json = "";
+        String line = reader.readLine();
+        while (line != null) {
+            json += line;
+            line = reader.readLine();
+        }
+        Gson gson = new Gson();
+        List<AddressData> contacts = gson.fromJson(json, new TypeToken<List<AddressData>>(){}.getType()); //List<AddressData>.class
+        return contacts.stream().map((g) -> new Object[] {g}).collect(Collectors.toList()).iterator();
+    }
+
+    @Test(dataProvider = "validAddressFromJson")
+    public void testsAddressCreation(AddressData contact) {
         Contacts before = app.address().all();
-        File photo = new File("src/test/resources/stru.png");
-        AddressData contact = new AddressData()
-                .withFirstname("Иван").withMiddlename("Иванович").withLastname("Иванов").withNickname("ivanov")
-                .withAddress("г. Москва").withHome("+7(495)111-11-11").withMobile("+7(915)111-11-11").withWork("+7(495)100-10-10")
-                .withEmail("ivanov@sitehome.ru").withEmail2("ivanov@sitehome.com").withEmail3("ivanov@sitehome.me")
-                .withHomepage("www.sitehome.ru").withGroup("test2").withPhoto(photo);
         app.address().create(contact);
         Contacts after = app.address().all();
 
