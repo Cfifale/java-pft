af346ba979c96a03ff1c845684382db4480deb75
cfifale <cfifale@yandex.ru>
2020-04-09 02:05:10 +0300

    Лекция 8.5

---
+++ mantis-tests/src/test/resources/config_inc.php
@@ -0,0 +1,11 @@
+<?php
+$g_hostname               = 'localhost';
+$g_db_type                = 'mysqli';
+$g_database_name          = 'bugtracker';
+$g_db_username            = 'root';
+$g_db_password            = '';
+
+$g_default_timezone       = 'Europe/Moscow';
+
+$g_crypto_master_salt     = 'TZOU5nKONX0+wxogPR1+f9jb7+v8QXmdpB3UWayH9iE=';
+$g_signup_use_captcha     = OFF;
