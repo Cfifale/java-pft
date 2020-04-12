b21d28a0989d27a038fe087613211ffc954541be
cfifale <cfifale@yandex.ru>
2020-04-13 01:17:30 +0300

    Лекция 8.6

--- mantis-tests/src/test/resources/config_inc.php
+++ mantis-tests/src/test/resources/config_inc.php
@@ -9,3 +9,6 @@ $g_default_timezone       = 'Europe/Moscow';
 
 $g_crypto_master_salt     = 'TZOU5nKONX0+wxogPR1+f9jb7+v8QXmdpB3UWayH9iE=';
 $g_signup_use_captcha     = OFF;
+
+$g_phpMailer_method = PHPMAILER_METHOD_SMTP;
+$g_smtp_host = 'localhost';
